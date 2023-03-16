e2e55ef727cf802e42242294f4981525165442ab
Laurent Cerveau <lcerveau@mmyneta.com>
2023-03-16 23:53:56 +0100

    [db] SQL schema tweaks

--- server/dbs/postgres/init_greendb.sql
+++ server/dbs/postgres/init_greendb.sql
@@ -36,30 +36,10 @@ CREATE TABLE IF NOT EXISTS token (
     expiration_date TIMESTAMPTZ,
     is_valid BOOL,
     value CHAR(256),
-    CONSTRAINT fk_user FOREIGN KEY(owner_uuid) REFERENCES user(uuid) 
+    CONSTRAINT fk_owner FOREIGN KEY(owner_uuid) REFERENCES user(uuid) 
 );
 
 CREATE INDEX IF NOT EXISTS idx_token_uuid ON token(uuid);
-
-CREATE TABLE IF NOT EXISTS session (
-    uuid CHAR(36) UNIQUE NOT NULL PRIMARY KEY,
-    object_version SMALLINT,
-    data_version SMALLINT,
-    created_at TIMESTAMPTZ,
-    updated_at TIMESTAMPTZ,
-    
-    termination_date TIMESTAMPTZ,
-    owner_uuid CHAR(36),
-    user_uuid CHAR(36),
-    device_uuid CHAR(36),
-    token_uuid CHAR(36),
-    is_new_user BOOLEAN NOT NULL,
-    is_new_device BOOLEAN NOT NULL
-);
-
-
-CREATE INDEX IF NOT EXISTS idx_session_uuid ON session(uuid);
-
 
 CREATE TABLE IF NOT EXISTS device (
     uuid CHAR(36)  UNIQUE NOT NULL PRIMARY KEY,
@@ -74,10 +54,36 @@ CREATE TABLE IF NOT EXISTS device (
     vendor_uuid CHAR(36),
     platform VARCHAR(8),  
     os_version VARCHAR(16),
-    last_connection_date TIMESTAMPTZ
+    last_connection_date TIMESTAMPTZ,
+    CONSTRAINT fk_owner FOREIGN KEY(owner_uuid) REFERENCES user(uuid) 
 );
 
 CREATE INDEX IF NOT EXISTS idx_device_uuid ON device(uuid);
+
+
+CREATE TABLE IF NOT EXISTS session (
+    uuid CHAR(36) UNIQUE NOT NULL PRIMARY KEY,
+    object_version SMALLINT,
+    data_version SMALLINT,
+    created_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ,
+    
+    termination_date TIMESTAMPTZ,
+    owner_uuid CHAR(36),
+    user_uuid CHAR(36),
+    device_uuid CHAR(36),
+    token_uuid CHAR(36),
+    is_new_user BOOLEAN NOT NULL,
+    is_new_device BOOLEAN NOT NULL,
+    CONSTRAINT fk_owner FOREIGN KEY(owner_uuid) REFERENCES user(uuid), 
+    CONSTRAINT fk_user FOREIGN KEY(user_uuid) REFERENCES user(uuid), 
+    CONSTRAINT fk_device FOREIGN KEY(device_uuid) REFERENCES device(uuid), 
+    CONSTRAINT fk_token FOREIGN KEY(token_uuid) REFERENCES token(uuid) 
+);
+
+
+CREATE INDEX IF NOT EXISTS idx_session_uuid ON session(uuid);
+
 
 CREATE TABLE IF NOT EXISTS greenhouse (
     uuid CHAR(36)  UNIQUE NOT NULL PRIMARY KEY,
@@ -91,7 +97,8 @@ CREATE TABLE IF NOT EXISTS greenhouse (
     latitude DOUBLE PRECISION,
     longitude DOUBLE PRECISION,
     display_name VARCHAR(256),
-    description TEXT
+    description TEXT,
+    CONSTRAINT fk_owner FOREIGN KEY(owner_uuid) REFERENCES user(uuid) 
 );
 
 CREATE INDEX IF NOT EXISTS idx_greenhouse_uuid ON greenhouse(uuid);
@@ -104,8 +111,11 @@ CREATE TABLE IF NOT EXISTS growbox (
     updated_at TIMESTAMPTZ,
 
     owner_uuid CHAR(36),
+    greenhouse_uuid CHAR(36),
     
-    display_name VARCHAR(256)
+    display_name VARCHAR(256),
+    CONSTRAINT fk_owner FOREIGN KEY(owner_uuid) REFERENCES user(uuid), 
+    CONSTRAINT fk_greenhouse FOREIGN KEY(greenhouse_uuid) REFERENCES greenhouse(uuid) 
 );
 
 CREATE INDEX  IF NOT EXISTS idx_growbox_uuid ON growbox(uuid);
@@ -119,7 +129,7 @@ CREATE TABLE IF NOT EXISTS plant (
 
     display_name VARCHAR(256),
     description TEXT,
-    bio_reference VARCHAR(256),
+    bio_reference VARCHAR(256)
 };
  
 
@@ -133,6 +143,8 @@ CREATE TABLE IF NOT EXISTS stem (
     updated_at TIMESTAMPTZ,
 
     owner_uuid CHAR(36),
+    plant_uuid CHAR(36),
+    growbox_uuid CHAR(36),
     
     display_name VARCHAR(256),
     description TEXT,
@@ -142,7 +154,12 @@ CREATE TABLE IF NOT EXISTS stem (
     sowing_date  TIMESTAMPTZ,
     growing_start_date TIMESTAMPTZ,
     flowering_date TIMESTAMPTZ,
-    harvest_date TIMESTAMPTZ
+    harvest_date TIMESTAMPTZ,
+
+
+    CONSTRAINT fk_owner FOREIGN KEY(owner_uuid) REFERENCES user(uuid), 
+    CONSTRAINT fk_growbox FOREIGN KEY(growbox_uuid) REFERENCES growbox(uuid), 
+    CONSTRAINT fk_plant FOREIGN KEY(plant_uuid) REFERENCES plant(uuid) 
 ); 
 
 CREATE INDEX IF NOT EXISTS idx_stem_uuid ON stem(uuid);
