cba19fbc59484b62d2163541e870cfe70773a653
twogreencows <moo@twogreencows.co>
2023-08-27 22:46:01 +0200

    [client] web client has now a 3 columns layout

---
+++ client/web/twogreencows/src/components/MainPage.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="mainpage">
+    <header class="header">Header</header>
+    <div class="content">
+      <div class="left-column">Left Column</div>
+      
+      <div class="main-zone">
+          <users></users> 
+      </div>
+    </div>
+    </div>
+</template>
+
+<script>
+import Users from './Users.vue'
+
+export default {
+    components: {
+        Users,
+    }
+}
+
+</script>
+
+<style >
+.mainpage {
+  display: flex;
+  flex-direction: column;
+  background-color:red;
+  height: 100vh;
+}
+
+.header {
+  background-color: lightgray;
+}
+
+.content {
+  display: flex;
+  flex: 1;
+}
+
+.left-column {
+  flex: 1;
+  background-color: #f0f0f0;
+}
+
+.main-zone {
+  flex: 3;
+  background-color: white;
+}
+</style>
+
---
+++ client/web/twogreencows/vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // options...
+    proxy: 'http://localhost:3000/'
+}
